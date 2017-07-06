3cec0ad3de4d070d774773d518f5532a0395165a
Ronald <rjdelrosario1@up.edu.ph>
2017-07-06 16:47:14 +0800

    the spaces

--- messenger/models.py
+++ messenger/models.py
@@ -8,25 +8,25 @@ from django.dispatch import receiver
 class MessageManager(models.Manager):
 	
 	def add_message(self,sender, thread, content):
-		return self.create(sender = sender, thread = thread, content = content)
+		return self.create(sender=sender, thread=thread, content=content)
 	
 	def getMessagebyThread(self,thread):
-		return self.filter(thread = thread)
+		return self.filter(thread=thread)
 
 class MessageThread(models.Model):
-    subject = models.CharField(max_length = 255, blank = True)
-    participants = models.ManyToManyField(User, related_name = 'message_threads')
-    when_created = models.DateTimeField(auto_now_add = True)
+    subject = models.CharField(max_length=255, blank=True)
+    participants = models.ManyToManyField(User, related_name='message_threads')
+    when_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
     	return (' {} '.format(self.subject))
 
 
 class Message(models.Model):
-	sender = models.ForeignKey(User, related_name = 'sender')
-	thread = models.ForeignKey(MessageThread, related_name = 'thread')
+	sender = models.ForeignKey(User, related_name='sender')
+	thread = models.ForeignKey(MessageThread, related_name='thread')
 	content = models.TextField()
-	when_created = models.DateTimeField(auto_now_add =True)
+	when_created = models.DateTimeField(auto_now_add=True)
 
 	objects = MessageManager()
 
@@ -34,14 +34,14 @@ class Message(models.Model):
 		return ('{}:{} '.format(self.thread, self.content))
 
 class Profile(models.Model):
-	owner = models.OneToOneField(User, related_name = 'owner')
+	owner = models.OneToOneField(User, related_name='owner')
 	thread = models.ManyToManyField(MessageThread, 
 		related_name = '_thread', through = 'ProfileThread')
 
 	def __str__(self):
 		return ('{} : {}  '.format(self.owner, self.thread.count()))
 
-@receiver(post_save, sender = User)
+@receiver(post_save, sender=User)
 def create_profile(sender, created, instance, **kwargs):
     """ Create a profile for every new user """
     if created:
@@ -49,6 +49,6 @@ def create_profile(sender, created, instance, **kwargs):
 
 
 class ProfileThread(models.Model):
-	user = models.ForeignKey(Profile, on_delete = models.CASCADE)
-	thread = models.ForeignKey(MessageThread, on_delete = models.CASCADE)
+	user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+	thread = models.ForeignKey(MessageThread, on_delete=models.CASCADE)
 	is_removed = models.BooleanField()
