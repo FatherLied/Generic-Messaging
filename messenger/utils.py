422874d70cf71e2a8ac2da06694724eb5b021d0b
Tilmo8 <santino_gaara@yahoo.com>
2017-07-13 11:10:20 +0800

    Resolve an issue regarding repeated filenames
    Considering that the archive is named based on the date it was created, a conflict will occur when tasked to archive multiple threads that were made on the same date.
    To resolve this, the code emulates how OS resolves multiple pastes of the same files in the directory.
    
    Note: This also allows duplicates in terms of content. Since 'publish_to_csv' returns a filename, the caller 'Archive' must immediately reference it.

--- messenger/utils.py
+++ messenger/utils.py
@@ -1,6 +1,16 @@
 from .models import MessageThread, Message
+from pathlib import Path
 import os
 
+"""
+Returns filename on use.
+
+Please store on call 
+    i.e. 'messenger.models.Archive.reference = publish_to_csv(thread)'
+
+    Note: Ensure that Archive does not have a reference before calling this else there will betwo files
+        with the same content
+"""
 def publish_to_csv(thread):
     # archive_date = thread.when_created.date()
     folder_path = 'messenger/archived'
@@ -12,6 +22,12 @@ def publish_to_csv(thread):
         # thread.when_created.date().strftime('%Y-%m-%d'))
 
     filename = 'archive-{}.csv'.format(thread.when_created.date().strftime('%Y-%m-%d'))
+    inc = 1
+
+    while Path(os.path.join(folder_path, filename)).exists():
+        filename = 'archive-{}[{}].csv'.format(thread.when_created.date().strftime('%Y-%m-%d'), inc)
+        inc += 1
+
     
     with open( os.path.join(folder_path, filename), 'w') as archive:
         thread_content = thread.content.all().order_by('when_created')
