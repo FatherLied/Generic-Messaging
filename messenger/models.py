f08984aaf6929a078b42f7b832a40350b37bbbdc
Tilmo8 <santino_gaara@yahoo.com>
2017-07-14 10:08:03 +0800

    modify some models

--- messenger/models.py
+++ messenger/models.py
@@ -47,8 +47,8 @@ class Profile(models.Model):
 	first_name = models.CharField(max_length=30, blank = False)
 	last_name = models.CharField(max_length=30, blank = False)
 	owner = models.OneToOneField(User, related_name='profile')
-	thread = models.ManyToManyField(MessageThread, 
-		related_name = '_thread', through = 'ProfileThread')
+	threads = models.ManyToManyField(MessageThread, 
+		related_name = 'profiles', through = 'ProfileThread')
 
 	def __str__(self):
 		return ('{} : {}  '.format(self.owner, self.thread.count()))
@@ -62,5 +62,5 @@ class Profile(models.Model):
 
 class ProfileThread(models.Model):
 	user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='thread_copies')
-	thread = models.ForeignKey(MessageThread, on_delete=models.CASCADE, related_name='copies')
+	threads = models.ForeignKey(MessageThread, on_delete=models.CASCADE, related_name='copies')
 	is_removed = models.BooleanField()
