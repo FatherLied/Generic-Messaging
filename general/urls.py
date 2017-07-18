1da9633ab4228e58ccd6882363ba081a8d5014e6
Ronald <rjdelrosario1@up.edu.ph>
2017-07-18 10:33:02 +0800

    apply pep-08 for general/urls.py

--- general/urls.py
+++ general/urls.py
@@ -18,25 +18,20 @@ from django.contrib import admin
 from django.conf import settings
 from django.contrib.auth import views as auth_views
 from dashboard import views as reg_views
-# from dashboard import views
 from dashboard.views import HomeView, SignUpView
 from dashboard.views import ThreadDetailsView, AddNewThreadView, JoinThreadsView
 from messenger.views import AddMessageView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^$', reg_views.home, name='home'),
     url(r'^$', HomeView.as_view(), name='home'),
-    url(r'^login/$', auth_views.login, {'template_name': 'dashboard/login.html'}, name='login'),
-    url(r'^logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'^login/$', auth_views.login, 
+        {'template_name': 'dashboard/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
     url(r'^signup/$', reg_views.signup, name='signup'),
-
-    # url(r'^send/$', send_message, name='send_message')
-
     url(r'^add/$', AddMessageView.as_view(), name='add_message'),
     url(r'^addnewthread/$', AddNewThreadView.as_view(), name='addnewthread'),
     url(r'^jointhreads/$', JoinThreadsView.as_view(), name='jointhreads'),
     url(r'^thread/(?P<pk>\d+)/$', ThreadDetailsView.as_view(), name='details'),
-
 ]
 
