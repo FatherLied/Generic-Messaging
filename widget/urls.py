b7326ca4d98002e47df2086b42cf1b831c82ad34
Pete <suarezpete53@gmail.com>
2017-07-25 10:17:53 +0800

    Setup widget, need fix views

--- widget/urls.py
+++ widget/urls.py
@@ -1,13 +1,19 @@
-from django.conf.urls import url 
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from dashboard import views as reg_views
+from django.conf import settings
 
-from .views import WidgetView 
-#,MessageWidget, AddMessageWidget, AllUsers, 
-# RetrieveMessageWidget
+from .views import WidgetView, AddMessageView, RetrieveMessage, AddNewThreadView, JoinThreadsView, ThreadDetailsView
 
 urlpatterns = [
     url(r'^home/$', WidgetView.as_view(), name='w_template'),
-    # url(r'^messagewidget/$', MessageWidget.as_view(),name=messagewidget),
-    #url(r'^addmessage/$' AddMessageWidget.as_view(), name=addmessagewidget).
-    #url(r'^allusers/$', AllUsers.as_view(), name='allusers'),
-    #url(r'^retrivemessage/$', RetrieveMessageWdget.as_view(), name='retrieve')
+    url(r'^login/$', auth_views.login, 
+        {'template_name': 'dashboard/login.html'}, name='login'),
+    url(r'^logout/$', auth_views.logout, {'next_page':'login'}, name='logout'),
+    url(r'^signup/$', reg_views.signup, name='signup'),
+    url(r'^addnewthread/$', AddNewThreadView.as_view(), name='addnewthread'),
+    url(r'^jointhreads/$', JoinThreadsView.as_view(), name='jointhreads'),
+    url(r'^thread/(?P<pk>\d+)/$', ThreadDetailsView.as_view(), name='details'),
+    url(r'^add/$', AddMessageView.as_view(), name='add_message'),
+    url(r'^retrieve/$', RetrieveMessage.as_view(), name='retrieve-message')
 ]
--- widget/views.py
+++ widget/views.py
@@ -1,11 +1,16 @@
-from django.shortcuts import render
 from django.views.generic.base import TemplateView
-from django.http import HttpResponse
 from django.views import View
+from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.contrib.auth import login, authenticate, logout
+from dashboard.forms import SignUpForm
+from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+from messenger.models import MessageThread, Message, Profile, Archive
 
-from messenger.models import MessageThread, Message, Profile
-# Create your views here.
+import time
 
+@method_decorator(login_required, name='dispatch')
 class WidgetView(TemplateView):
     template_name = 'widget/simpletemplate.html'
 
@@ -18,52 +23,98 @@ class WidgetView(TemplateView):
     def dispatch(self, request, *args, **kwargs):
         return super(WidgetView,self).dispatch(self.request, *args, **kwargs)
 
-# class CreateMessageThread(View):
-#     pass
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.refresh_from_db()
+            user.save()
+            password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=password)
+            login(request, user)
+            return redirect('home')
+    else:
+        form = SignUpForm()
+    return render(request, 'dashboard/signup.html', {'form': form})
 
-# class MessageWidget(View):
-#     def dispatch(self, request, pk):
-#         thisthreads = get_object_or_404(MessageThread, pk=pk)
-#         context = {
-#             'messages': Message.objects.filter(
-#                 thread=thisthreads).order_by('when_created'),
-#             'next_url': reverse('details',args=(pk,))
-#         }
-#         return HttpResponse('')
 
-# class AddMessageWidget(View):
-#     def post(self, request):
-#         w_content = request.POST.get('w_content')
-#         w_thread_id = request.POST.get('w_thread_id')
-#         thread = MessageThread.objects.get(pk=w_thread_id)
-#         message = Message.objects.add_message(content=content,
-#             thread=thread, sender=request.user)
-#         return JsonResponse({
-#             'threadId': message.thread_id,
-#             'content': message.content,
-#             'when': t,
-#             'sender':message.sender.username,
-#             'sender_pk': message.sender.pk
-#             })
+class ThreadDetailsView(View):
+    def dispatch(self, request, pk):
+        thisthreads = get_object_or_404(MessageThread, pk=pk)
+        context = {
+            'threads':  MessageThread.objects.filter(
+                participants=request.user).order_by('-when_created'),
+            'users' : Profile.objects.all(),
+            'thread_id':pk,
+            'allthreads':MessageThread.objects.exclude(participants=request.user),
+            'messages': Message.objects.filter(
+                thread=thisthreads).order_by('when_created'),
+            'next_url': reverse('details', args=(pk,))
+        }
+        return render(request, 'dashboard/details.html', context=context)
 
-# class AllUsers(View):
-#     def dispatch(self, request, pk):
-#         users = Profile.objects.all()
-#         return JsonResponse({'users':users})
+class AddNewThreadView(View):
+    def post(self, request):
+        subject = request.POST['subject']
+        exists = MessageThread.objects.filter(subject=subject)
+        if exists:
+            return HttpResponse(' ')
+        thread = MessageThread.objects.create(subject=subject)
+        thread.participants.add(request.user)
+        thread_url = reverse('details',args=(thread.pk,))
+        return JsonResponse({'subject':thread.subject,'thread_url':thread_url})
 
-# class RetrieveMessageWidget(View):
 
-#     def get(self, request, *args, **kwargs):
-#         latest_id = request.GET['latest_id']
-#         thread_id = request.GET['threadId']
-#         messages = Message.objects.filter(id__gt=latest_id,
-#             thread__id=thread_id)
-#         context = {}
-#         context['messages'] = []
-#         for message in messages:
-#             context['messages'].append({'pk':message.pk,
-#                 'content':message.content,
-#                 'when':message.when_created.strtime("%B %d, %Y, %-I:%M %p"),
-#                 'sender':message.sender.pk,
-#                 'sender_pk':message.sender.pk})
-#         return JsonResponse({'objects':context})
+class JoinThreadsView(View):
+    def post(self, request):
+        subject = request.POST['subject']
+        print(subject)
+        thread = MessageThread.objects.get(subject=subject)
+        thread1 = MessageThread.objects.filter(subject=subject).exclude(participants=request.user)
+        if not thread:
+            return JsonResponse({'status':'error1','context':'The thread does not exists.'})
+        if not thread1:
+            return JsonResponse({'status':'error2','context':'You are already part of the thread'})
+        
+        # thread = MessageThread.objects.filter(subject=subject)
+        thread.participants.add(request.user)
+        thread_url = reverse('details',args=(thread.pk,))
+        
+        return JsonResponse({'subject':thread.subject,'thread_url':thread_url,'status':'success'})
+
+class AddMessageView(View):
+    def post(self, request):
+        content = request.POST.get('content')
+        thread_id = request.POST.get('thread_id')
+        thread = MessageThread.objects.get(pk=thread_id)
+        message = Message.objects.add_message(content=content, 
+            thread=thread, sender=request.user)
+        t = message.when_created.strftime("%B %d, %Y, %-I:%M %p")
+        return JsonResponse({'pk': message.pk,
+            'threadId': message.thread_id,
+            'content': message.content,
+            'when': t,
+            'sender': message.sender.username,
+            'sender_pk': message.sender.pk
+            })
+
+    def download_csv(request, archive_obj):
+        pass
+
+
+class RetrieveMessage(View):
+
+    def get(self, request, *args, **kwargs):
+        latest_id = request.GET['latestId']
+        thread_id = request.GET['threadId']
+        messages = Message.objects.filter(id__gt=latest_id, thread__id=thread_id)
+        context = {}
+        context['messages'] = []
+        for message in messages:
+            context['messages'].append({'pk': message.pk, 
+                'content': message.content, 
+                'when': message.when_created.strftime("%B %d, %Y, %-I:%M %p"), 
+                'sender': message.sender.username, 
+                'sender_pk': message.sender.pk})
+        return JsonResponse({'objects': context})
