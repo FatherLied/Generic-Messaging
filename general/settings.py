c8d1d7ff647934296f037d9e66023e6b66c64123
Tilmo8 <santino_gaara@yahoo.com>
2017-07-28 15:11:02 +0800

    Add serverside stuff

--- general/settings.py
+++ general/settings.py
@@ -130,6 +130,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
